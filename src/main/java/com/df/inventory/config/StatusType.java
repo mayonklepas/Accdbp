e58638c137bbc8f5b989f01375d0735fc014db45
imonks21 <imonks21@gmail.com>
2025-06-17 10:28:16 +0700

    update status

--- src/main/java/com/df/inventory/config/StatusType.java
+++ src/main/java/com/df/inventory/config/StatusType.java
@@ -12,8 +12,6 @@ public class StatusType {
 
         public static String ORDERING="ordering";
         public static String APPROVED="approved";
-        public static String PREPARING="preparing";
-        public static String SENDING="sending";
         public static String FINISH="finish";
                 
         
